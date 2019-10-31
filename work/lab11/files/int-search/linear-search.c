a96a8f3e2a89399777d930708a41e5135b7befbb
Parampreet Parmar <Pparmar2@binghamton.edu>
2019-10-31 17:40:33 -0400

    submitted lab11

---
+++ work/lab11/files/int-search/linear-search.c
@@ -0,0 +1,10 @@
+#include "int_compare.h"
+#include <stdlib.h>
+/** Return index of element in a[nElements]; < 0 if not found. */
+int
+search_for_element(int a[], int nElements, int element)
+{
+  //@TODO add your code here to meet above spec.
+  int *p = bsearch(&element, a, nElements, sizeof(a[0]), int_compare);
+  return (p) ? p - a : -1;	
+}
